7de8fd535e244b90ff1cf92f62e1dadbf183ef53
Chenyang Shi <chenyang9608@qq.com>
2018-11-12 09:37:39 +0800

    update data path

--- simple_deeplearning/main.py
+++ simple_deeplearning/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author: LogicJake
 # @Date:   2018-10-29 18:53:24
-# @Last Modified time: 2018-11-06 10:55:55
+# @Last Modified time: 2018-11-12 09:34:11
 from preprocessing import Preprocessing
 from model_main import MainModel
 import os
@@ -17,7 +17,8 @@ def create_dir():
 if __name__ == '__main__':
     create_dir()
 
-    preprocessing = Preprocessing('dataset/input.csv', 'dataset/output.csv')
+    preprocessing = Preprocessing(
+        '../dataset/input.csv', '../dataset/output.csv')
     preprocessing.reformat()
 
     main_model = MainModel('transformed_dataset/final.csv')
