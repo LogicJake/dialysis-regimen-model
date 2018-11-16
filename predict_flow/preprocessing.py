84fbf7882864214dbfce6baae8881b53658f77b2
Chenyang Shi <chenyang9608@qq.com>
2018-11-16 15:03:19 +0800

    delete unused

--- predict_flow/preprocessing.py
+++ predict_flow/preprocessing.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Author: LogicJake
 # @Date:   2018-11-16 10:10:26
-# @Last Modified time: 2018-11-16 11:22:27
+# @Last Modified time: 2018-11-16 15:03:00
 import os
 import pandas as pd
 import numpy as np
-from imblearn.over_sampling import SMOTE
 
 
 def create_dir():
@@ -28,21 +27,11 @@ class Preprocessing(object):
         df = df.dropna(subset=['mode', 'machine',
                                'anti_type', 'anti_first', 'flow'])
 
-        # # merge output
-        # df['mm'] = df['machine'].str.cat(df['mode'], sep=':')
-        # df = df.drop(['machine', 'mode'], axis=1)
-        # df['anti_first'] = df['anti_first'].map(lambda x: str(x))
-        # df['anti'] = df['anti_type'].str.cat(df['anti_first'], sep=':')
-        # df = df.drop(['anti_first', 'anti_type'], axis=1)
-
-        # df = df[['flow', 'mm', 'anti']]
         df = df[['flow', 'machine', 'mode', 'anti_type', 'anti_first']]
         df = pd.get_dummies(
             df, columns=['machine', 'mode', 'anti_type', 'anti_first'], prefix_sep='+')
         print(df.columns.values.tolist())
         df.to_csv('transformed_dataset/data.csv', index=False, header=False)
-
-        return df
 
 if __name__ == '__main__':
     preprocessing = Preprocessing('../dataset/output.csv')
